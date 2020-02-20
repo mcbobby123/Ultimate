a0ed4f8ed56ad1f00c256e6abf0a809d99277d04
Kelly Dance21 <KDance21@tcastudent.org>
2020-02-20 14:34:32 -0600

    visibility fix

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
 const express = require('express');
 const app = express();
-const port = 4000;
+const server = require('http').createServer(app);
+const port = 80;
+const io = require('socket.io')(server);
 
-app.use(express.static('frontend/build'));
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+'/frontend/temp.html');
+});
 
-app.use('*',(req,res)=>{
-    res.send('hello world');
-})
+let players = [];
+let board;
 
-app.listen(port,()=>console.log(`Booted on Port ${port}`));
+
+io.on('connection',(socket)=>{
+    let team = 0;
+    if(players.length === 0){
+        board = new Array(9).fill(0).map(()=>new Array(9).fill(0));
+    }
+    if(!players.some(p=>p.team===1)) team=1;
+    if(!team&&!players.some(p=>p.team===2)) team=2;
+    players.push({
+        socket,
+        team
+    });
+    socket.on('disconnect',()=>{
+        players = players.filter(p=>p.socket.id!==socket.id);
+        console.log(players);
+    });
+    socket.on('play',([b,i])=>{
+        console.log(b,i);
+    })
+    console.log(players);
+});
+
+server.listen(port,()=>console.log(`Booted on Port ${port}`));
